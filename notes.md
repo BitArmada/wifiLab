79220a0d0e51688ada5e94081013f3aee6c40aad
BitArmada <73617255+BitArmada@users.noreply.github.com>
2022-11-16 04:11:24 +0000

    making acurate

---
+++ notes.md
@@ -0,0 +1,7 @@
+
+dish:
+Horn antennas
+yagi antenna
+
+phased array:
+Inverted-F antenna
--- script.js
+++ script.js
@@ -4,22 +4,32 @@ var ctx = canv.getContext('2d');
 canv.width = 500;
 canv.height = 500;
 
-var sheetWidth = 5;
+var diameter = 15;
+const circumference = Math.PI*diameter;
 
 var pedals = 8;
-var focalPoint = 1;
+var focalPoint = 5;
+const parabola = (x)=>(Math.pow(x,2)/(4*focalPoint));
+const depth = parabola(diameter/2);
 
 function cutArea(x){
     return (Math.PI/pedals)*(Math.pow(x,3)/(24*Math.pow(focalPoint,2)));
 }
 
+function inverseCutArea(x){
+    return Math.cbrt((x*pedals*24*Math.pow(focalPoint,2))/Math.PI)
+}
+
 function draw(){
     ctx.fillStyle = 'white';
     ctx.fillRect(0,0,canv.width, canv.height)
+    ctx.save();
     ctx.translate(canv.width/2, canv.height/2);
-
+    
     const nangle = (Math.PI*2)/pedals;
-    const unit = (sheetWidth/2)/(canv.width/2);
+    const trad = inverseCutArea((circumference)/pedals);
+    const unit = trad/(canv.width/2);
+
     for(var n = 0; n < pedals; n++){
         ctx.rotate(nangle);
         ctx.strokeStyle = 'black';
@@ -42,6 +52,17 @@ function draw(){
     ctx.beginPath();
     ctx.arc(0, 0, canv.width/2, 0, 2 * Math.PI);
     ctx.stroke();
+
+    ctx.restore();
+    ctx.fillStyle = 'black';
+    ctx.font = "20px Verdana";
+    ctx.fillText(`${round(trad, 1000)} x ${round(trad, 1000)}`, 0,20);
+    ctx.fillText(`d: ${diameter} h: ${round(depth, 1000)} c: ${round(circumference, 1000)}`, 0, 40 );
+    ctx.fillText(`f: ${round(focalPoint, 1000)} p: ${pedals}`, 0, 60 );
+}
+
+function round(v, n){
+    return Math.round(v*n)/n;
 }
 
 draw();
